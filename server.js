309eda5f360f2c3d2317421f5062dcf71ae16ed6
amit verma <amitv87@gmail.com>
2018-01-25 17:40:17 +0530

    change default port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 var self = {};
 self.host = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
-self.port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+self.port = process.env.OPENSHIFT_NODEJS_PORT || 10001;
 
 var WebSocketServer = require('uws').Server;
 var qs = require('url');
